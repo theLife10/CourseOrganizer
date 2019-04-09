9261b298f23f1426fba1d73c86ee84e7366d6784
theLife10 <jdgarcia9@miners.utep.edu>
2019-04-09 13:35:41 -0600

    skeleton code for database

--- app/src/main/java/cs4330/cs/utep/courseorganizer/DBhelper.java
+++ app/src/main/java/cs4330/cs/utep/courseorganizer/DBhelper.java
@@ -16,7 +16,7 @@ public class DBhelper extends SQLiteOpenHelper {
     private static final String KEY_CLASSDAY = "classday";
     private static final String KEY_ASSIGNMENTNAME = "assignmentname";
     private static final String KEY_DUEDATE = "duedate";
-    private static final String KEY_COURSE = "done";
+    private static final String KEY_COURSE = "course";
 
     public DBhelper(Context context){
         super(context,DB_NAME,null,DB_VERSION);
@@ -24,13 +24,32 @@ public class DBhelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String sql = "CREATE TABLE " + SCHOOL_TABLE + "("
+                + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + KEY_INSTRUCTORSNAME + " TEXT, "
+                + KEY_PHONE + "REAL,"
+                + KEY_EMAIL + "TEXT,"
+                + KEY_CLASSDAY + "TEXT,"
+                + KEY_ASSIGNMENTNAME + " TEXT,"
+                + KEY_DUEDATE + " TEXT,"
+                + KEY_COURSE + " TEXT"
+                + ")";
+        db.execSQL(sql);
 
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + SCHOOL_TABLE);
+        onCreate(db);
     }
+
+    public void deleteData(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(SCHOOL_TABLE, KEY_ID +  "=" + id, null) ;
+        db.close();
+    }
+
+
 }
